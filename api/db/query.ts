2cc5faa8a944cd36eff40f9ff80058999a41af94
Omar Maldonado <v-omaldonado@expediagroup.com>
2023-04-18 16:35:53 -0600

    conecxion a db transcrita a Ts

--- api/db/query.ts
+++ api/db/query.ts
@@ -1,23 +1,26 @@
-import { connectionDB } from './connection'
-import { RespuestaDB } from '@api/utils/response'
-import { ResDB } from '@api/models/respuestas.model'
+import { connectionDB } from "./connection"
+import { RespuestaDB } from "@api/utils/response"
+import { ResDB } from "@api/models/respuestas.model"
 
-const queryDB = ( query: string ): Promise<ResDB> => {
-    return new Promise((resolve, reject) => {
-        connectionDB.query( query, (err, results, fields) => {
-            if(err) reject(RespuestaDB.fallida(err))
-            resolve(RespuestaDB.exitosa(results as []))
-        })
+const queryDB = (query: string): Promise<ResDB> => {
+  return new Promise((resolve, reject) => {
+    connectionDB.query(query, (err, results, fields) => {
+      if (err) reject(RespuestaDB.fallida(err))
+      resolve(RespuestaDB.exitosa(results))
     })
+  })
 }
 
-const queryDBPlaceHolder = ( query: string, placeHolders: Array<string | number> ): Promise<ResDB> => {
-    return new Promise((resolve, reject) => {
-        connectionDB.query( query, placeHolders, (err, results, fields) => {
-            if(err) reject(RespuestaDB.fallida(err))
-            resolve(RespuestaDB.exitosa(results as []))
-        })
+const queryDBPlaceHolder = (
+  query: string,
+  placeHolders: Array<string | number>
+): Promise<ResDB> => {
+  return new Promise((resolve, reject) => {
+    connectionDB.query(query, placeHolders, (err, results, fields) => {
+      if (err) reject(RespuestaDB.fallida(err))
+      resolve(RespuestaDB.exitosa(results))
     })
+  })
 }
 
 export { queryDB, queryDBPlaceHolder }
--- api/utils/response.ts
+++ api/utils/response.ts
@@ -1,41 +1,56 @@
-import { ResController, ResDB } from '@api/models/respuestas.model'
+import { ResController, ResDB } from "@api/models/respuestas.model"
+import { RowDataPacket, OkPacket, ResultSetHeader } from "mysql2"
+import Query from "mysql2/typings/mysql/lib/protocol/sequences/Query"
 
 class RespuestaDB {
-    
-    static exitosa( data: [] ): ResDB {
-        return {
-            data,
-            error: false
-        }
+  static exitosa(
+    data:
+      | RowDataPacket[]
+      | RowDataPacket[][]
+      | OkPacket
+      | OkPacket[]
+      | ResultSetHeader
+  ): ResDB {
+    return {
+      data,
+      error: false,
     }
+  }
 
-    static fallida( data: object ): ResDB {
-        return {
-            data,
-            error: true
-        }
+  static fallida(data: Query.QueryError): ResDB {
+    return {
+      data,
+      error: true,
     }
+  }
 }
 
 class RespuestaController {
-    
-    static exitosa( status: number, mensaje: string, data: [] | object ): ResController {
-        return {
-            status,
-            mensaje,
-            error: false,
-            data,
-        }
+  static exitosa(
+    status: number,
+    mensaje: string,
+    data: [] | object
+  ): ResController {
+    return {
+      status,
+      mensaje,
+      error: false,
+      data,
     }
+  }
 
-    static fallida( status: number, mensaje: string, data: [] | object ): ResController {
-        return {
-            status,
-            mensaje,
-            error: true,
-            data,
-        }
+  static fallida(
+    status: number,
+    mensaje: string,
+    data: [] | object
+  ): ResController {
+    return {
+      status,
+      mensaje,
+      error: true,
+      data,
     }
+  }
 }
 
 export { RespuestaDB, RespuestaController }
