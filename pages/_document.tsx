89630b6bf8b176a09333229486f631c365986a19
Omar Maldonado <omaldonado@apexsystems.com>
2023-10-02 17:16:28 -0600

    agrego favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="https://static.wixstatic.com/media/86d1f6_788b0729ac7d46109935bd4b157b9f48%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/86d1f6_788b0729ac7d46109935bd4b157b9f48%7Emv2.png"
+          type="image/png"
+        />
+      </Head>
       <body>
         <Main />
         <div id="modal"></div>
